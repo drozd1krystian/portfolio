2f8ace55aef37f11e4bea5a6710053c926dbbe25
Krystian <drozd1krystian@gmail.com>
2020-06-01 20:08:56 +0200

    form - mobile support

--- dist/js/SmoothScrolling.js
+++ dist/js/SmoothScrolling.js
@@ -4,27 +4,11 @@ $("#nav_bar a").on("click", function (event) {
     event.preventDefault();
 
     const hash = this.hash;
-    function detectmob() {
-      if (window.innerWidth <= 800 && window.innerHeight <= 600) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    if (detectmob()) {
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800
-      );
-    } else {
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800
-      );
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
   }
 });
