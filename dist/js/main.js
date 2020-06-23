7036778c15bb3fa648e11ffb0fdba65bd8a21919
Krystian <drozd1krystian@gmail.com>
2020-06-23 18:14:52 +0200

    changed skills section

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,3 +10,24 @@ window.onscroll = function () {
   }
   prevScrollPos = currentScrollPos;
 };
+
+let cards = document.querySelectorAll(".card");
+let slideBtn = document.querySelectorAll(".line");
+let container = document.querySelector("#skills .flex_container div");
+
+skills.addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (target.tagName !== "SPAN") return;
+  let width = cards[0].offsetWidth;
+  let id = target.id;
+  let amount = -width * id;
+
+  container.style.transform = `translate3d(${amount}px, 0px, 0px)`;
+  Array.from(slideBtn).forEach((el) => {
+    if (el.classList.contains("activeSlide"))
+      el.classList.remove("activeSlide");
+  });
+
+  target.classList.add("activeSlide");
+});
