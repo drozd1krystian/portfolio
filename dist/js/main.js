3278700bf34acd8560ce61b84b13c0c95b3d1e6c
Krystian <drozd1krystian@gmail.com>
2021-02-04 14:49:45 +0100

    removed unnecessary css file

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,20 @@
+// Smooth Scrolling
+$("#nav_bar a, #about a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
+  }
+});
+
+// Hide Nav
+
 let prevScrollPos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
@@ -10,3 +27,19 @@ window.onscroll = function () {
   }
   prevScrollPos = currentScrollPos;
 };
+
+// Burger Menu
+const navSlide = (() => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".links");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+    burger.classList.toggle("toggle");
+  });
+})();
+
+$(".links a").on("click", function () {
+  $(".links").removeClass("active");
+  $(".burger").removeClass("toggle");
+});
